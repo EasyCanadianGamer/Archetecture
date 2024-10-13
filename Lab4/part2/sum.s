f4a5536ab70250c527543e35deb194ff77cbc384
EasyCanadianGamer <merajue1@tcnj.edu>
2024-10-13 10:43:43 -0400

    asm

--- Lab4/part2/sum.s
+++ Lab4/part2/sum.s
@@ -1,9 +1,9 @@
-	.arch armv8-a
-	.file	"sum.c"
-	.text
-	.align	2
-	.global	sum
-	.type	sum, %function
+.arch armv8-a
+.file	"sum.c"
+.text
+.align	2
+.global	sum
+.type	sum, %function
 sum:
 .LFB0:
 	.cfi_startproc
@@ -22,67 +22,68 @@ sum:
 	//=====================================
 	// TODO: begin your assembly code here
 
+	// store n 
+	str x0, [sp, 8]
 
-// store n 
-	str x0,[sp,8]
-
-// initialize and store 0
+	// initialize and store res = 0
 	mov x1, #0
+	str x1, [sp, 16]
 
-	str x1,[sp,16]
+	// initialize i = 1 and store
+	mov x2, #1
+	str x2, [sp, 24]
 
-	//initialize i and store
-	mov x1, #1
-	str x2,[sp, 24]
+	// if n <= 0, return -1
+	ldr x0, [sp, 8]
+	cmp x0, #0
+	ble EndWithMinusOne
 
-	// if n <= 0
 Loop:
-    // Load i into x2
-    ldr x2, [sp, 24]
+	// Load i into x2
+	ldr x2, [sp, 24]
 
-    // Load n into x0
-    ldr x0, [sp, 8]
+	// Load n into x0
+	ldr x0, [sp, 8]
 
-    // Compare i and n
-    cmp x2, x0
-    bgt END  // If i > n, exit loop
+	// Compare i and n
+	cmp x2, x0
+	bgt End  // If i > n, exit loop
 
-    // Load res into x1
-    ldr x1, [sp, 16]
+	// Load res into x1
+	ldr x1, [sp, 16]
 
-    // res = res + i
-    add x1, x1, x2
+	// res = res + i
+	add x1, x1, x2
 
-    // Store updated res back to [sp, 16]
-    str x1, [sp, 16]
+	// Store updated res back to [sp, 16]
+	str x1, [sp, 16]
 
-    // Increment i by 1
-    add x2, x2, #1
+	// Increment i by 1
+	add x2, x2, #1
 
-    // Store updated i back to [sp, 24]
-    str x2, [sp, 24]
+	// Store updated i back to [sp, 24]
+	str x2, [sp, 24]
 
-    // Repeat the loop
-    b Loop
-
-
+	// Repeat the loop
+	b Loop
 
 End:
-    // Load the result res into x0 (return value)
+	// Load the result res into x0 (return value)
+	ldr x0, [sp, 16]
+	add	sp, sp, #32	// deallocate stack frame
+	.cfi_def_cfa_offset 0
+	ret
 
-	ldr x0,[sp, 16]
+EndWithMinusOne:
+	// Return -1 if n <= 0
+	mov x0, #-1
+	add	sp, sp, #32	// deallocate stack frame
+	ret
 
-
-
-
-
-
+	//=====================================
 	// TODO: end your assembly code here
 	//=====================================
 
-	add	sp, sp, 32	// deallocate stack frame
-	.cfi_def_cfa_offset 0
-	ret
 	.cfi_endproc
 .LFE0:
 	.size	sum, .-sum
